2c4e5878379203af2ec5cc05ecb75ab860d2456a
bigrocketgames <admin@bigrocketgames.com>
2018-04-11 08:40:33 -0700

    section 3, lecture 21

--- commonJS/src/image_viewer.js
+++ commonJS/src/image_viewer.js
@@ -1,6 +1,6 @@
 import '../styles/image_viewer.css';
 
 const image = document.createElement('img');
-image.src = 'http://lorempixel.com/400/400';
+image.src = 'http://lorempixel.com/400/400/animals';
 
 document.body.appendChild(image);
